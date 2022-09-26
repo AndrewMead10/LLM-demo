703e76871585c12265863ed637f7559190daaef0
andrew mead <ammead@umass.edu>
2022-09-26 11:00:07 -0400

    idk

--- reference/model/test.py
+++ reference/model/test.py
@@ -1,9 +1,15 @@
 from model import SmallTextGenerator
 import torch
+import transformers
 
-x = torch.randint(0, 1000, (1, 100))
-model = SmallTextGenerator()
-y = model(x)
+# x = torch.randint(0, 1000, (1, 100))
+# model = SmallTextGenerator()
+# y = model(x)
+
+# print(model)
+# print(sum([p.numel() for p in model.parameters()]))
+# print(y.shape)
+
+model = transformers.OPTModel.from_pretrained("facebook/opt-125m")
+
 print(model)
-print(sum([p.numel() for p in model.parameters()]))
-print(y.shape)
